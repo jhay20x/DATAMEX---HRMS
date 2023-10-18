97f0e3b4d632c5077f77f27b1268de98b593b62f
Jhayjay Gonzales <Jhay20x@outlook.com>
2023-10-18 08:18:38 +0800

    Gonzales Oct 18 - Update

--- HRMS/EMPLOYEE/Form1.Designer.vb
+++ HRMS/EMPLOYEE/Form1.Designer.vb
@@ -127,7 +127,7 @@ Partial Class Form1
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = " "
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
